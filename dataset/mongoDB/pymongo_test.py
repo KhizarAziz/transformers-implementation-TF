280fc38c972b34f995295b8e0a06e9b99d2c22e8
Innovative <IS@blabla.com>
2022-01-04 11:01:29 +0000

    mongoDB insertion pipeline added

--- dataset/mongoDB/pymongo_test.py
+++ dataset/mongoDB/pymongo_test.py
@@ -18,12 +18,12 @@ if __name__ == "__main__":
     client = pymongo.MongoClient('mongodb://localhost:27017') # connect to db
     # print(client)
 
-    db = client['PyMongoDB'] # create db with name if doesnt exist else connect to ths db
+    db = client['CC_db'] # create db with name if doesnt exist else connect to ths db
 
-    collection = db['creative_sa']
+    collection = db['all_news_articles']
     collection.create_index("text", unique=True)
 
-    jsonls_source_path = Path("mongo_source/")
+    jsonls_source_path = Path("data_source")
     dat_backup_dir = Path("data_backup/")
     for jsonl_path in jsonls_source_path.rglob('*.jsonl'): # get all jsonl files from dir and all sub directories of this path.
         with open(jsonl_path) as jsonl_file: # open each jsonl
@@ -32,13 +32,14 @@ if __name__ == "__main__":
                 try:
                     article = json.loads(line)
                     collection.insert_one(article)
+                    #print('SUCCESS:',len(article['text']))
                 except Exception as e:
-                    print("some issue in file ", jsonl_path, e)
+                   # print('FAIL:', len(article['text']))
+                    print("Issue: {} || Size: {} ".format(str(e)[:20], len(article['text'])))
                     pass
         r = shutil.move(jsonl_path,dat_backup_dir.joinpath(jsonl_path.name))
-        print('File Moved to Backup dir, New path is: ',r)
-
-    print('Converting Publish_dates from dateString to DateObject (ISO).')
+        print('All articles done.. source Moved to dir: {} '.format(r))
+    print('Converting Publish_dates from dateString to DateObject (ISO) \n\n\n.')
     # convert datestring to dateobject
     collection.update_many({},[{
         "$project": {
